986ce01da3821c016536f4c35fd5f51827320bc8
Harlan <113073322+eych00@users.noreply.github.com>
2022-11-24 21:59:41 -0600

    PROJECT1

---
+++ LOGIN/login.js
@@ -0,0 +1,11 @@
+var signup = document.getElementById('signUp');
+var signin = document.getElementById('signIn');
+function showSignup() 
+    signin.style.display = "none";
+    signup.style.display = "block";
+            
+function hideSignup() 
+    signin.style.display = "block";
+    signup.style.display = "none";
+            
+           
